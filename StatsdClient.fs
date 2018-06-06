223c96b60b5c0bc96bf0591b9ae2b3c16e2c215c
cboudereau <cboudereau@gmail.com>
2018-06-06 17:25:00 +0200

    init

---
+++ StatsdClient.fs
@@ -0,0 +1,32 @@
+﻿module StatsdClient
+
+open System.Net.Sockets
+
+type Metric = private Metric of string
+
+let counter name value = sprintf "%s:%i|c" name value |> Metric
+let gauge name value = sprintf "%s:%i|g" name value |> Metric
+let timer name value = sprintf "%s:%i|ms" name value |> Metric
+
+let empty = Metric ""
+
+let private (|Empty|_|) (Metric x) = if System.String.IsNullOrWhiteSpace(x) then None else Some ()
+
+let (+) x y = 
+    match x, y with
+    | Empty, x' | x', Empty -> x'
+    | Metric x', Metric y' -> sprintf "%s\n%s" x' y' |> Metric
+
+let udp hostname (port:uint16) = new System.Net.Sockets.UdpClient(hostname, int port)
+
+type [<Struct>] Sender = private Sender of (Metric -> Async<unit>)
+
+let sender (client:UdpClient) = 
+    fun (Metric m) -> 
+        let bytes = System.Text.Encoding.ASCII.GetBytes(m:string)
+        client.SendAsync(bytes, bytes.Length)
+        |> Async.AwaitTask
+        |> Async.Ignore
+    |> Sender
+
+let send (Sender f) m = f m
